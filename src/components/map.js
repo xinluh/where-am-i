68ab58b307bcb1b97ce931799c7db186c6988a19
Xinlu Huang <xinlu.h@gmail.com>
2020-03-13 08:07:48 -0600

    wait for data to load before showing map
    looks like mobile doesn't load properly if map is shown before data is
    loaded

--- src/components/map.js
+++ src/components/map.js
@@ -49,6 +49,12 @@ const MapChart = ({ locations }) => {
     })
   }, [])
 
+  const locationsFiltered = locations
+    .filter(l => l.lat)
+    .filter(l => !l.justPassingBy)
+
+  if (locationsFiltered.length === 0) return null
+
   return (
     <ErrorBoundary errorMessage="Failed to load map">
       <div>
@@ -66,94 +72,81 @@ const MapChart = ({ locations }) => {
             }
           </Geographies>
 
-          {locations
-            .filter(l => l.lat)
-            .filter(l => !l.justPassingBy)
-            .map(
-              (
-                {
-                  nightAt,
-                  lat,
-                  lon,
-                  isFuture,
-                  date,
-                  dayOfTrip,
-                  stoppingPoints,
-                },
-                idx
-              ) => (
-                <React.Fragment key={nightAt}>
-                  <Marker
-                    coordinates={[lon, lat]}
-                    onMouseEnter={() => {
-                      setTooltipContent(
-                        `Day ${dayOfTrip} (${date}) ${nightAt}` +
-                          (isFuture
-                            ? " (date/location subject to change!)"
-                            : "")
-                      )
-                    }}
-                    onMouseLeave={() => setTooltipContent("")}
+          {locationsFiltered.map(
+            (
+              { nightAt, lat, lon, isFuture, date, dayOfTrip, stoppingPoints },
+              idx
+            ) => (
+              <React.Fragment key={nightAt}>
+                <Marker
+                  coordinates={[lon, lat]}
+                  onMouseEnter={() => {
+                    setTooltipContent(
+                      `Day ${dayOfTrip} (${date}) ${nightAt}` +
+                        (isFuture ? " (date/location subject to change!)" : "")
+                    )
+                  }}
+                  onMouseLeave={() => setTooltipContent("")}
+                >
+                  <g
+                    fill="none"
+                    stroke={isFuture ? "#808080" : "#FF5533"}
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    transform="translate(-12, -24)"
                   >
-                    <g
-                      fill="none"
-                      stroke={isFuture ? "#808080" : "#FF5533"}
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      transform="translate(-12, -24)"
-                    >
-                      <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
-                    </g>
-                    <text
-                      textAnchor="middle"
-                      stroke={isFuture ? "#808080" : "#FF5533"}
-                      style={{
-                        fontFamily: "system-ui",
-                        fill: "#5D5A6D",
-                        fontSize: 10,
-                      }}
-                      y={-10}
-                    >
-                      {idx + 1}
-                    </text>
-                  </Marker>
+                    <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                  </g>
+                  <text
+                    textAnchor="middle"
+                    stroke={isFuture ? "#808080" : "#FF5533"}
+                    style={{
+                      fontFamily: "system-ui",
+                      fill: "#5D5A6D",
+                      fontSize: 10,
+                    }}
+                    y={-10}
+                  >
+                    {idx + 1}
+                  </text>
+                </Marker>
 
-                  {latLonLookup &&
-                    stoppingPoints &&
-                    stoppingPoints.trim() !== "" &&
-                    stoppingPoints.split("|").map(loc => {
-                      return (
-                        latLonLookup[loc] && (
-                          <Marker
-                            key={loc}
-                            coordinates={[
-                              latLonLookup[loc].lon,
-                              latLonLookup[loc].lat,
-                            ]}
-                            onMouseEnter={() => {
-                              setTooltipContent(
-                                `Visit: ${loc} - Day ${dayOfTrip} (${date})` +
-                                  (isFuture
-                                    ? " (date/location subject to change!)"
-                                    : "")
-                              )
-                            }}
-                            onMouseLeave={() => setTooltipContent("")}
-                          >
-                            <circle
-                              r={5}
-                              fill={isFuture ? "#808080" : "#FF5533"}
-                              stroke="#fff"
-                              strokeWidth={2}
-                            />
-                          </Marker>
-                        )
+                {latLonLookup &&
+                  stoppingPoints &&
+                  stoppingPoints.trim() !== "" &&
+                  stoppingPoints.split("|").map(loc => {
+                    return (
+                      latLonLookup[loc] && (
+                        <Marker
+                          key={loc}
+                          coordinates={[
+                            latLonLookup[loc].lon,
+                            latLonLookup[loc].lat,
+                          ]}
+                          onMouseEnter={() => {
+                            setTooltipContent(
+                              `Visit: ${loc} - Day ${dayOfTrip} (${date})` +
+                                (isFuture
+                                  ? " (date/location subject to change!)"
+                                  : "")
+                            )
+                          }}
+                          onMouseLeave={() => setTooltipContent("")}
+                        >
+                          <circle
+                            r={5}
+                            fill={isFuture ? "#808080" : "#FF5533"}
+                            stroke="#fff"
+                            strokeWidth={2}
+                          />
+                        </Marker>
                       )
-                    })}
-                </React.Fragment>
-              )
-            )}
+                    )
+                  })}
+              </React.Fragment>
+            )
+          )}
         </ComposableMap>
         <ReactTooltip>{tooltipContent}</ReactTooltip>
       </div>
