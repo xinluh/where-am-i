62a8b2410aff7926f98b4f90e2824c2292616dca
Xinlu Huang <xinlu.h@gmail.com>
2021-11-22 12:45:31 -0700

    Updates

---
+++ component---src-pages-404-js-52f7884533f5e6561f49.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkxinluh_where_am_i=self.webpackChunkxinluh_where_am_i||[]).push([[883],{9616:function(e,t,a){a.r(t);var n=a(7294),l=a(7367),u=a(3751);t.default=function(e){var t=e.data,a=e.location,i=t.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:i},n.createElement(u.Z,{title:"404: Not Found"}),n.createElement("h1",null,"Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-52f7884533f5e6561f49.js.map
